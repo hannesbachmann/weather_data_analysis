a6e935cd35ba3326e788a05651321d406c447c38
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-09 21:24:32 +0200

    add description for var self.__data

--- dataset.py
+++ dataset.py
@@ -6,9 +6,10 @@ class DataSet:
         self.__REL_PATH = 'data_TT_TU_MN009'
         self.__file_type = 'csv'
 
-        self.__data = []
+        self.__data = []        # array for the rows of the data table
 
     def open_file(self):
+        self.__data = []
         with open(self.__REL_PATH + '.' + self.__file_type) as data_csv:                             # open the file
             csv_reader_obj = csv.reader(data_csv, delimiter=',')     # create the csv reader object
             for row in csv_reader_obj:
