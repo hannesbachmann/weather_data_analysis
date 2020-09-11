2ff3035c0ddb0192c45f871b28ad123bd642fad4
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-11 21:56:09 +0200

    writing down the workflow

--- weather_analysis.py
+++ weather_analysis.py
@@ -2,3 +2,9 @@ import display
 import interpolation
 import dataset
 
+# inread dataset
+# convert time steps into 15 min intervals
+# find hottest and coldest temperatures of each year ->store in .csv
+# plot the temperature for the hottest and coldest days of each year over time of day
+# one last analysis
+
