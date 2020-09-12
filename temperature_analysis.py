4e9d81980c2f52f7960d5414f946fca97bdfe059
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-12 10:11:02 +0200

    find coldest temperature and when it occure

--- temperature_analysis.py
+++ temperature_analysis.py
@@ -10,3 +10,13 @@ class Temperature:
 
     def find_coldest_temp_and_time(self, complete_data):
         self.__coldest_temp = float(complete_data[1][3])
+        coldest_times = [complete_data[1][2]]
+        for row in complete_data[1:]:
+            if float(row[3]) < self.__coldest_temp:
+                self.__coldest_temp = float(row[3])
+                coldest_times = [row[2]]
+            elif float(row[3]) == self.__coldest_temp:
+                coldest_times.append(row[2])
+        print(f"coldest temp: {self.__coldest_temp}, times: {coldest_times}")
+
+
