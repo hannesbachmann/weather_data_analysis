c4cb01fb203e487eaaabd303340e7785d6ccfe56
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-12 10:37:07 +0200

    find hottest temperature and when it occure also writing getter for class var's

--- temperature_analysis.py
+++ temperature_analysis.py
@@ -1,22 +1,54 @@
+from dataset import DataSet
 
 
 class Temperature:
     def __init__(self):
-        self.__hottest_temp = None
-        self.__coldest_temp = None
+        self.__coldest_row = None
+        self.__hottest_row = None
 
         self.__hottest_day = None
         self.__coldest_day = None
 
-    def find_coldest_temp_and_time(self, complete_data):
-        self.__coldest_temp = float(complete_data[1][3])
-        coldest_times = [complete_data[1][2]]
+    def get_hottest_row(self):
+        return self.__hottest_row
+
+    def get_coldest_row(self):
+        return self.__coldest_row
+
+    def get_hottest_day(self):
+        return self.__hottest_day
+
+    def get_coldest_day(self):
+        return self.__coldest_day
+
+    def find_coldest_temp_in_row(self, complete_data):
+        coldest_temp = float(complete_data[1][3])
+        self.__coldest_row = [complete_data[1]]
         for row in complete_data[1:]:
-            if float(row[3]) < self.__coldest_temp:
-                self.__coldest_temp = float(row[3])
-                coldest_times = [row[2]]
-            elif float(row[3]) == self.__coldest_temp:
-                coldest_times.append(row[2])
-        print(f"coldest temp: {self.__coldest_temp}, times: {coldest_times}")
+            if float(row[3]) < coldest_temp:
+                coldest_temp = float(row[3])
+                self.__coldest_row = [row]
+            elif float(row[3]) == coldest_temp:
+                self.__coldest_row.append(row)
+        print(f"coldest temp: {coldest_temp}, times: {self.__coldest_row}")
+
+    def find_hottest_temp_in_row(self, complete_data):
+        hottest_temp = float(complete_data[1][3])
+        self.__hottest_row = [complete_data[1]]
+        for row in complete_data[1:]:
+            if float(row[3]) < hottest_temp:
+                hottest_temp = float(row[3])
+                self.__hottest_row = [row]
+            elif float(row[3]) == hottest_temp:
+                self.__hottest_row.append(row)
 
 
+
+
+if __name__ == '__main__':
+    D = DataSet()
+    TEMP = Temperature()
+
+    D.open_file()
+    mydata = D.get_data_set()
+    TEMP.find_coldest_temp_in_row(mydata)
