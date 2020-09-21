7332914b658a5a2de38c6bbbbc0cbf8065438a66
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-21 21:49:41 +0200

    separate gauss process from plotting

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_hottest_and_coldest_days(hottest_days, coldest_days):
@@ -19,5 +20,22 @@ def plot_hottest_and_coldest_days(hottest_days, coldest_days):
     ax[1].set_xlabel('time in h')
     ax[1].set_ylabel('temperature in °C')
     ax[1].legend()
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_trend_prediction(x, y, x_unknown, y_unknown, sigma):
+    fig = plt.figure(figsize=(6, 3), dpi=100)
+    plt.plot(x, y, '-', label='true data')
+    plt.plot(x_unknown, np.exp(y_unknown) - 30, 'r-', label='prediction')  # expected future data
+    plt.fill_between(x_unknown, np.exp(y_unknown - sigma) - 30, np.exp(y_unknown + sigma) - 30,
+                     color='0.85')  # possible space
+    plt.xlim(2015, 2021)  # axis limitation
+    plt.xticks(np.arange(2015, 2021, 1))  # x axis scale
+    plt.ylim(-20, 40)  # axis limitation
+    plt.vlines([2020], -20, 40, '0.6', '--')  # dividing line
+    plt.title('Temperature-Trend')
+    plt.xlabel('time (year)')
+    plt.ylabel('temperature in °C')
     fig.tight_layout()
     plt.show()
