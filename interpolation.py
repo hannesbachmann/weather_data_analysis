d6c6cac052c6517d00966e66026fee198f00ff63
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-21 21:20:29 +0200

    calculating and plotting the trend of the future temperature

--- interpolation.py
+++ interpolation.py
@@ -1,5 +1,8 @@
 from scipy.interpolate import CubicSpline
 import numpy as np
+import scipy.spatial
+import math
+import matplotlib.pyplot as plt
 
 
 def perform_interpolation(complete_data):
@@ -49,3 +52,59 @@ def perform_interpolation(complete_data):
         spline_index += 1
     return finer_complete_file
 
+
+def covFunc(d):
+    return 0.8 * np.exp(-np.abs(np.sin(np.pi * d)) / 0.5 - np.abs(d / 25.) ** 2 - 2.5) + \
+           (0.2 - 0.01) * np.exp(-(np.abs(np.sin(np.pi * d / 4)) / 0.2)) + 0.01 * np.exp(-np.abs(d / 45.))
+
+
+def covMat(x1, x2, covFunc, noise=0):
+    cov = covFunc(scipy.spatial.distance_matrix(np.atleast_2d(x1).T, np.atleast_2d(x2).T))
+    if noise:
+        np.fill_diagonal(cov, np.diag(cov) + noise)
+    return cov
+
+
+def gauss_process(complete_data):
+    x = np.arange(2015., 2020., 5 / 4383.)
+    print(len(x))
+
+    y = []
+    full_y = []
+    for i in range(len(complete_data)):
+        if i % 10 == 0 and complete_data[i][2][0:4] != '2020':
+            y.append(float(complete_data[i][3]) + 30)
+        elif i % 10 == 0:
+            full_y.append(float(complete_data[i][3]))
+    print(len(y))
+
+    x_known = x
+    y_known = np.log(y)
+    x_unknown = np.arange(2020, 2021, 5 / 4383.)  # range der unbekannten
+    full_x = np.arange(2020, 2020.152, 5 / 4383.)
+    print(len(full_x), len(full_y))
+
+    Ckk = covMat(x_known, x_known, covFunc, noise=0.02)
+    Cuu = covMat(x_unknown, x_unknown, covFunc, noise=0.00)
+    CkkInv = np.linalg.inv(Ckk)
+    Cuk = covMat(x_unknown, x_known, covFunc, noise=0)
+    m = np.mean(y_known)
+    y_unknown = m + np.dot(np.dot(Cuk, CkkInv), y_known - m)
+    sigmaPrior = np.sqrt(np.mean(np.square(y_known)))
+    sigma = sigmaPrior * np.sqrt(np.diag(Cuu - np.dot(np.dot(Cuk, CkkInv), Cuk.T)))
+
+    y = [value - 30 for value in y]
+    fig = plt.figure(figsize=(6, 3), dpi=100)
+    plt.plot(x, y, '-')
+
+    plt.plot(full_x, full_y)
+    plt.plot(x_unknown, np.exp(y_unknown) - 30, 'r-')                # expected future data
+    plt.fill_between(x_unknown, np.exp(y_unknown - sigma) - 30, np.exp(y_unknown + sigma) - 30, color='0.85')     # possible space
+    plt.xlim(2015, 2021)          # axis limitation
+    plt.xticks(np.arange(2015, 2021, 2))    # x axis scale
+    plt.ylim(-20, 40)                # axis limitation
+    plt.vlines([2020], -20, 40, '0.6', '--')      # dividing line
+    plt.title('Temperature-Trend')
+    plt.xlabel('time (year)')
+    plt.ylabel('temperature in °C')
+    plt.show()
--- weather_analysis.py
+++ weather_analysis.py
@@ -1,5 +1,5 @@
 import display
-from interpolation import perform_interpolation
+from interpolation import perform_interpolation, gauss_process
 from dataset import DataSet, TempData
 from temperature_analysis import Temperature
 from display import plot_hottest_and_coldest_days
@@ -12,7 +12,7 @@ def run():
 
     Dataset.open_file()
     complete_file = Dataset.get_data_set()
-    finer_file = perform_interpolation(complete_file)
+    finer_file = perform_interpolation(complete_data=complete_file)
 
     years = Temp.separate_into_years(finer_file)
     hottest_days = []
@@ -28,6 +28,8 @@ def run():
     legend = Dataset.get_legend()
     Tempdata.store_extrema(legend, hottest_temp_year, coldest_temp_year)
     plot_hottest_and_coldest_days(hottest_days, coldest_days)
+
+    gauss_process(complete_file[1:])
 
 
 if __name__ == '__main__':
