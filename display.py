e8247dd04bc959ce1db77b61634c2dc600a64109
hannesbachmann <hannes-bachmann@t-online.de>
2020-09-21 15:07:23 +0200

    plotting the hottest and coldest temperature over day and time of day

--- display.py
+++ display.py
@@ -2,19 +2,22 @@ import matplotlib.pyplot as plt
 
 
 def plot_hottest_and_coldest_days(hottest_days, coldest_days):
-    fig, ax1 = plt.subplots(figsize=(6.5, 4))
+    fig, ax = plt.subplots(2, 1)
+    fig.suptitle('hottest and coldest days of each year')
     for day in hottest_days:
         day_temp = [float(col[3]) for col in day[0]]
         day_time = [0.25 * i for i in range(93)]
-        ax1.plot(day_time, day_temp)
-    plt.xlabel('time over one day')
-    plt.ylabel('temperature')
+        ax[0].plot(day_time, day_temp, label=day[0][0][2][0:4])
+    ax[0].set_xlabel('time in h')
+    ax[0].set_ylabel('temperature in °C')
+    ax[0].legend()
 
-    fig, ax2 = plt.subplots(figsize=(6.5, 4))
     for day in coldest_days:
         day_temp = [float(col[3]) for col in day[0]]
         day_time = [0.25 * i for i in range(len(day[0]))]
-        ax2.plot(day_time, day_temp)
-    plt.xlabel('time over one day')
-    plt.ylabel('temperature')
+        ax[1].plot(day_time, day_temp, label=day[0][0][2][0:4])
+    ax[1].set_xlabel('time in h')
+    ax[1].set_ylabel('temperature in °C')
+    ax[1].legend()
+    fig.tight_layout()
     plt.show()
